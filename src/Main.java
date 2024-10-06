5c4c57944842dc58ad895bc09423f8c9d2381c95
Isliam Dulatov <157202261+Islym123@users.noreply.github.com>
2024-10-06 12:06:08 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,37 @@
+package org.example;
+
 import java.util.Random;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        Random rand = new Random();
-        int max = 0;
+        int arraySize = 1000;
+        int maxValue = 10000;
+        int[] numbers = new int[arraySize];
 
-        // Создание массива из случайных 1000 чисел
-        int[] array = new int [1000];
-        for (int i = 0;i < 1000;i++){
-            array[i] = rand.nextInt(10000);
+        // Заполнение массива случайными числами
+        Random random = new Random();
+        for (int i = 0; i < arraySize; i++) {
+            numbers[i] = random.nextInt(maxValue) + 1;  // Генерация чисел от 1 до 10000
         }
 
-        //
-        for (int i = 0;i < 1000;i++){
-            if (array[i]%14==0){
-                for (int j = 0;j<1000;j++){
-                    for (int n = 999;n>-1;n--){
-                        if(array[i]==array[j]*array[n]){
-                            if (max<array[i])max = array[i];
-                            break;
-                        }
-                    }
+        // Поиск числа кратного 14, чётного и являющегося произведением двух чисел из массива
+        boolean found = false;  // Флаг для отслеживания, найдено ли число
+        for (int i = 0; i < arraySize; i++) {
+            for (int j = i + 1; j < arraySize; j++) {
+                int product = numbers[i] * numbers[j];
+
+                // Проверка условия: кратность 14, чётность и произведение двух чисел
+                if (product <= maxValue && product % 14 == 0 && product % 2 == 0) {
+                    System.out.println("Найдено число: " + product + " = " + numbers[i] + " * " + numbers[j]);
+                    found = true;
+                    break; // Прерывание после нахождения первого подходящего числа
                 }
             }
+            if (found) break;  // Прерывание внешнего цикла, если число найдено
         }
-        System.out.println(max);
+
+        if (!found) {
+            System.out.println("Число, удовлетворяющее условиям, не найдено.");
+        }
     }
 }
